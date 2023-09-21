a82b2f8ff5f9b9d310aaed38ffb69ba0607aa685
Shifa <119953244+shifakhousain@users.noreply.github.com>
2023-09-21 08:58:51 -0700

    Create add font.py
        # Extract Marcellus-Regular from google font in the form of .ttf format

---
+++ add font.py
@@ -0,0 +1,36 @@
+
+# Import
+import pygame
+
+# Initialize
+pygame.init()
+
+# Create Window/Display
+width,height = 1280 , 720
+window = pygame.display.set_mode((width, height))
+pygame.display.set_caption("My Awasome Game")
+
+#Initialize clock for Fps
+fps = 30
+clock = pygame.time.Clock()
+
+# Main loop
+start = True
+while start:
+    # Get Events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            start = False
+            pygame.QUIT()
+
+    # Apply Logics
+    window.fill((255,255,255))
+    font = pygame.font.Font('../Resources/Marcellus-Regular.ttf',100)
+    # Extrct Marcellus-Regular from google font in the form of .ttf formate 
+    text = font.render("My Awesome Game",True,(50, 50, 50))
+    window.blit(text,(350, 300))
+
+    #Update Display
+    pygame.display.update()
+    # Set fps
+    clock.tick(fps)
